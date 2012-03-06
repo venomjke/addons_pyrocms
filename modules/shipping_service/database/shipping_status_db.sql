10cc6c0d1e7a9cb9bec26755d54d8268fa0a9663
alex.strigin <apstrigin@gmail.com>
2012-03-06 22:37:09 +0400

    fix database info

--- modules/shipping_service/database/shipping_status_db.sql
+++ modules/shipping_service/database/shipping_status_db.sql
@@ -3,6 +3,6 @@ CREATE TABLE IF NOT EXISTS `{DATABASE_NAME}` (
   `name` varchar(64) NOT NULL,
   `comment` varchar(1024) NOT NULL,
   `default` TINYINT NOT NULL DEFAULT  '0',
-  `notification` TINYINT( 4 ) NOT NULL DEFAULT  '0'
+  `notification` TINYINT( 4 ) NOT NULL DEFAULT  '0',
     PRIMARY KEY (`id`)
 ) ENGINE=MyISAM DEFAULT CHARSET=utf8;
