4512bd71a811de809c6db2f8e9d3f2eb78b9fa07
kRen0 <nekonoorigato@gmail.com>
2012-03-13 10:23:13 +0400

    fix installation

--- modules/shipping_service/database/shipping_status_db.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS `{DATABASE_NAME}` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` varchar(64) NOT NULL,
-  `comment` varchar(1024) NOT NULL,
-  `default` TINYINT NOT NULL DEFAULT  '0',
-  `notification` TINYINT( 4 ) NOT NULL DEFAULT  '0',
-    PRIMARY KEY (`id`)
-) ENGINE=MyISAM DEFAULT CHARSET=utf8;
